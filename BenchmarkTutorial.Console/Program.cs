243e824923b6ddf7ed8257539ba0f9ffa6d7db94
FEA <furkanemrealtintas@gmail.com>
2023-05-07 04:05:44 +0300

    Add project files.

---
+++ BenchmarkTutorial.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkTutorial.Console;
+
+
+
+BenchmarkRunner.Run<Tester>();
+
+Console.ReadLine();
+
+
+[SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net60)]
+[SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net70)]
+[SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net80)]
+[MemoryDiagnoser]
+public class Tester
+{
+    [Benchmark]
+    public int HandleWithNonSealedClass()
+    {
+        NotSealedClass notSealedClass = new NotSealedClass();
+        return notSealedClass.Handle(5,10);
+    }
+    [Benchmark]
+    public int HandleWithSealedClass()
+    {
+        SealedClass sealedClass = new SealedClass();
+        return sealedClass.Handle(5,10);
+
+    }
+}
+
+
---
+++ BenchmarkTutorial.Console/SealedClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkTutorial.Console
+{
+    public sealed class SealedClass
+    {
+        public int Handle(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    public class NotSealedClass
+    {
+        public virtual int Handle(int num1,int num2)
+        {
+
+            return num1 + num2;
+        }
+    }
+}
